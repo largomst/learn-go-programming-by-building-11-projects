850471ba90de8d087a3dc22e370dda96b39b7a68
largomst <dingrui.zhen@gmail.com>
2023-04-18 21:02:28 +0800

    fix: Handle -> HandleFunc

--- go-server/main.go
+++ go-server/main.go
@@ -32,7 +32,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
-	http.Handle("/hello", helloHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8090", nil); err != nil {
