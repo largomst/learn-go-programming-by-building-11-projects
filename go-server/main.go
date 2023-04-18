2c356df7b5001d46ab5ad2c5114e218632e6fc4b
largomst <dingrui.zhen@gmail.com>
2023-04-18 21:01:18 +0800

    fix: add feedline

--- go-server/main.go
+++ go-server/main.go
@@ -10,7 +10,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "Parseform() error: %v", err)
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprint(w, "Name = %s\nAddress = %s\n", name, address)
