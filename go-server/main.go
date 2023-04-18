20bb8d6962a850fee8e318663d22172a8682c47c
largomst <dingrui.zhen@gmail.com>
2023-04-18 21:03:07 +0800

    fix: 8090 -> 8080

--- go-server/main.go
+++ go-server/main.go
@@ -35,7 +35,7 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8090", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
